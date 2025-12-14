162c4a2837617776358b50f9de3391f86c0a9f62
Fabian Scheidt <mail@fabian-scheidt.com>
2025-12-14 17:47:57 +0100

    chore: initial commit

---
+++ Video Wall Utility/Video_Wall_UtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_Wall_UtilityApp.swift
+//  Video Wall Utility
+//
+//  Created by Fabian Scheidt on 14.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Video_Wall_UtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Video Wall UtilityTests/Video_Wall_UtilityTests.swift
@@ -0,0 +1,17 @@
+//
+//  Video_Wall_UtilityTests.swift
+//  Video Wall UtilityTests
+//
+//  Created by Fabian Scheidt on 14.12.25.
+//
+
+import Testing
+@testable import Video_Wall_Utility
+
+struct Video_Wall_UtilityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
